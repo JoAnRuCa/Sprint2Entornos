2575d64d56da5096dd1d20b688bbca7029066335
JoAnRuCa <joseantrc1998@gmail.com>
2025-02-07 11:42:53 +0100

    Hola Mundo Jose Pozo

---
+++ HolaMundoJoseManuelDelPozoLopez/src/HolaMundo/HolaMundoJoseManuelDelPozoLopez.java
@@ -0,0 +1,7 @@
+package HolaMundo;
+
+public class HolaMundoJoseManuelDelPozoLopez {
+		public static void main(String[] args) {
+			System.out.println("HolaMundo Jose Manuel Del Pozo Lopez");
+		}
+}
